780443551c21588ac9a3c5e5100b23eccd13d873
iniubongisemin <eugeneinie@gmail.com>
2026-05-06 17:41:57 +0100

    Conditional Prompts

--- OpenAI_API/prompt_engg.py
+++ OpenAI_API/prompt_engg.py
@@ -36,7 +36,7 @@ Crafting a well-structured prompt with delimiters
 """
 
 prompt = """Summarize the text delimited by triple backticks into bullet points.
-TEXT GOES HERE"""
+```TEXT GOES HERE```"""
 response = get_response (prompt)
 
 "USING FORMATTED F-STRINGS"
@@ -44,4 +44,69 @@ response = get_response (prompt)
 
 text = "This is a sample text to summarize"
 prompt = f"""Summarize the text delimited by triple backticks into bullet points.
-{text}"""
+```{text}```"""
+
+
+"USING DELIMITED PROMPTS WITH F-STRINGS"
+story = ""
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Create a prompt that completes the story
+prompt = f"Complete story delimited by triple backticks ```{story}```"
+
+# Get the generated response 
+response = get_response(prompt)
+
+print("\n Original story: \n", story)
+print("\n Generated story: \n", response)
+
+
+"BUILDING SPECIFIC AND PRECISE PROMPTS"
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Create a request to complete the story
+prompt = f"Complete the story delimited with triple backticks with only two paragraphs in the style of Shakespeare {story}"
+
+# Get the generated response
+response = get_response(prompt)
+
+print("\n Original story: \n", story)
+print("\n Generated story: \n", response)
+
+
+"CUSTOM OUTPUT FORMAT"
+text = """Once upon a time in a quaint little village, there lived a curious young boy named
+David. David was [...]"""
+
+instructions = """You will be provided with a text delimited by triple backticks. Generate a
+suitable title for it."""
+
+output_format = """Use the following format for the output:
+- Text: <text we want to title>
+- Title: <the generated title>"""
+
+prompt = instructions + output_format + f"```{text}```"
+print(get_response(prompt))
+
+"""
+RESPONSE: 
+- Text: Once upon a time in a quaint little village, there lived a curious young boy [...]
+- Title: The Extraordinary Adventure of David and the Mysterious Book
+"""
+
+
+"CONDITIONAL PROMPTS"
+prompt = f"""You will be provided with a text delimited by triple backticks.
+If the text is written in English, suggest a suitable title for it.
+Otherwise, write 'I only understand English'.```{text}```"""
+
+print(get_response(prompt))
+
+">>> INCORPORATING MULTIPLE CONDITIONS"
+
+text = "In the heart of the forest, sunlight filters through the lush green canopy, creating a tranquil atmosphere [...]"
+prompt = f"""You will be provided with a text delimited by triple backticks.
+If the text is written in English, check if it contains the keyword 'technology'.
+If it does, suggest a suitable title for it, otherwise, write 'Keyword not found'.```{text}```"""
+
+print(get_response(prompt))
