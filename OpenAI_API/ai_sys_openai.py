8344bd50d1c01d05a6631000a4bc039883206582
iniubongisemin <eugeneinie@gmail.com>
2026-06-21 23:10:05 +0100

    FEAT: Token Limits & Batching Messages

--- OpenAI_API/ai_sys_openai.py
+++ OpenAI_API/ai_sys_openai.py
@@ -40,17 +40,37 @@ print(get_response("gpt-4o-mini", {"role": "user", "content": "List ten holiday 
 
 
 "BATCHING MESSAGES"
-measurements = []
 client = OpenAI(api_key="<OPENAI_API_TOKEN>")
 
+measurements = []
 messages = []
 # Provide a system message and user messages to send the batch
 messages.append({
     "role": "system",
-    "content": """Convert each of the measurements from kilometers to miles and present the results in a table containing both the original and converted measurements."""
+    "content": """Convert each of the measurements from kilometres to miles and present the results in a table containing both the original and converted measurements."""
 })
 # Append measurements to the message
-[messages.append({"role": "user", "content": i}) for i in measurements]
+[messages.append({"role": "user", "content": str(i)}) for i in measurements]
 
 response = get_response(messages)
 print(response)
+
+
+"SETTING TOKEN LIMITS"
+import tiktoken
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+input_message = {"role": "user", "content": "I'd like to buy a shirt and a jacket. Can you suggest two color pairings for these items?"}
+
+# Use tiktoken to create the encoding for your model
+encoding = tiktoken.encoding_for_model("gpt-4o-mini")
+# Check for the number of tokens
+num_tokens = len(encoding.encode(input_message.get("content")))
+
+# Run the chat completions function and print the response
+if num_tokens <= 100:
+    response = client.chat.completions.create(model="gpt-4o-mini", messages=[input_message])
+    print(response.choices[0].message.content)
+else:
+    print("Message exceeds token limit")
+
+
