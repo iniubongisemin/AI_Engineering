ba6d7a69fcf9bc9c93f1409590a8f0ac570f67d9
iniubongisemin <eugeneinie@gmail.com>
2026-06-23 23:24:18 +0100

    FEAT: Functions & External APIs in AI Systems

--- OpenAI_API/ai_sys_openai.py
+++ OpenAI_API/ai_sys_openai.py
@@ -74,3 +74,153 @@ else:
     print("Message exceeds token limit")
 
 
+"USING THE TOOLS PARAMETER"
+
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+message_listing = []
+function_definition = ""
+
+response= client.chat.completions.create(
+    model="gpt-4o-mini",
+    # Add the message
+    messages=message_listing,
+    # Add your function definition
+    tools=function_definition
+)
+
+# Print the response
+print(response.choices[0].message.tool_calls[0].function.argument)
+
+
+"BUILDING A FUNCTION DICTIONARY"
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Define the function parameter type
+function_definition[0]['function']['parameters']['type'] = "object"
+
+# Define the function properties
+function_definition[0]['function']['parameters']['properties'] = {
+    "title": {
+        "type": "string",
+        "description": "Title of publication"
+    },
+    "year": {
+        "type": "string",
+        "description": "Year of publication"
+    }
+}
+
+response = get_response(messages, function_definition)
+print(response)
+
+
+"EXTRACTING THE RESPONSE"
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+response = get_response(messages, function_definition)
+
+# Define the function to extract the data dictionary
+def extract_dictionary(response):
+  return response.choices[0].message.tool_calls[0].function.arguments
+
+# Print the data dictionary
+print(extract_dictionary(response))
+
+
+"PARALLEL FUNCTION CALLING"
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Append the second function
+function_definition.append({
+        'type': 'function', 
+        'function':{
+            'name': 'reply_to_review', 
+            'description': 'Return the review reply as a reply property.', 
+            'parameters': {'type': "object", 'properties': {'reply': {
+                'type': 'string',
+                'description': 'Reply'
+            }}}}})
+
+response = get_response(messages, function_definition)
+
+# Print the response
+print(response)
+
+
+"SETTING A SPECIFIC FUNCTION"
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+model = {}
+
+response= client.chat.completions.create(
+    model=model,
+    messages=messages,
+    # Add the function definition
+    tools=function_definition,
+    # Specify the function to be called for the response
+    tool_choice={
+        'type': 'function',
+        'function': {'name': 'extract_review_info'}
+    }
+)
+
+# Print the response
+print(response.choices[0].message.tool_calls[0].function.arguments)
+
+
+"AVOIDING INCONSISTENT RESPONSES"
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Modify the messages
+messages.append({
+    "role": "system",
+    "content": "Don't make assumptions about what values used in the functions. Don't make up values to fill the response with."
+})
+
+response = get_response(messages, function_definition)
+
+print(response)
+
+
+"DEFINING A FUNCTION WITH EXTERNAL APIS"
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Define the function to pass to tools
+function_definition = [{
+    "type": "function",
+    "function" : {
+        "name": "get_exchange_rate",
+        "description": "This function uses the ExchangeRate API and takes as input one currency code, returning the response with the requested exchange rate information",
+        "parameters": {
+            "type": "object", 
+            "properties": {
+                "currency_code": {
+                    "type": "string",
+                    "description": "GBP"
+                }
+            } 
+        }, 
+        "result": {"type": "string"}
+    }
+}]
+
+response = get_response(function_definition)
+print(response)
+
+
+"CALLING AN EXERNAL API"
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+print_response = ""
+
+# Call the Chat Completions endpoint 
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {
+      "role": "system",
+      "content": ""
+    },
+    
+    {"role": "user", "content": "I'd like to know the current exchange rates for the Euro."}],
+  tools=function_definition)
+
+print_response(response)
