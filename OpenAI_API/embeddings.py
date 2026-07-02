fb9bd9a971ce8039b4fbecb19f4492f5bbe34633
iniubongisemin <eugeneinie@gmail.com>
2026-07-02 17:51:56 +0100

    FEAT: Embedding More Detailed Descriptions & Classifying Review Sentiments

--- OpenAI_API/embeddings.py
+++ OpenAI_API/embeddings.py
@@ -198,3 +198,53 @@ for hit in hits:
   print(product['title'])
 
 
+"EMBEDDING RESTAURANT REVIEWS"
+sentiments = [{'label': 'Positive'},
+              {'label': 'Neutral'},
+              {'label': 'Negative'}]
+
+reviews = ["The food was delicious!",
+           "The service was a bit slow but the food was good",
+           "The food was cold, really disappointing!"]
+# Create a list of class descriptions from the sentiment labels
+class_descriptions = [sentiment["label"] for sentiment in sentiments][0]
+
+# Embed the class_descriptions and reviews
+class_embeddings = create_embeddings(class_descriptions)
+review_embeddings = create_embeddings(reviews)
+
+
+"CLASSIFYING REVIEW SENTIMENT"
+# Define a function to return the minimum distance and its index
+def find_closest(query_vector, embeddings):
+  distances = []
+  for index, embedding in enumerate(embeddings):
+    dist = distance.cosine(query_vector, embedding)
+    distances.append({"distance": dist, "index": index})
+  return min(distances, key=lambda x: x["distance"])
+
+for index, review in enumerate(reviews):
+  # Find the closest distance and its index using find_closest()
+  closest = find_closest(review_embeddings[index], class_embeddings)
+  # Subset sentiments using the index from closest
+  label = sentiments[closest['index']]['label']
+  print(f'"{review}" was classified as {label}')
+
+
+"EMBEDDING MORE DETAILED DESCRIPTIONS"
+# Extract and embed the descriptions from sentiments
+class_descriptions = [description["description"] for description in sentiments]
+class_embeddings = create_embeddings(class_descriptions)
+review_embeddings = create_embeddings(reviews)
+
+def find_closest(query_vector, embeddings):
+  distances = []
+  for index, embedding in enumerate(embeddings):
+    dist = distance.cosine(query_vector, embedding)
+    distances.append({"distance": dist, "index": index})
+  return min(distances, key=lambda x: x["distance"])
+
+for index, review in enumerate(reviews):
+  closest = find_closest(review_embeddings[index], class_embeddings)
+  label = sentiments[closest['index']]['label']
+  print(f'"{review}" was classified as {label}')
